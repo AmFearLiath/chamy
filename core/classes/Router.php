fa7fac16fcc028425f923e438bf890a0eaef6343
AmFearLiath <amfearliath@gmx.de>
2023-09-07 01:37:08 +0200

    Update Router.php

--- core/classes/Router.php
+++ core/classes/Router.php
@@ -1,164 +1,59 @@
 <?php
 
 namespace Core\Classes;
+use Core\Manager\RouteManager;
 
 class Router
 {
-    // Array der Routen
-    protected $routes = [];
+    // Objekt der Klasse RouteManager
+    protected $routeManager;
 
-    // Array der Parameter
-    protected $params = [];<?php
-
-namespace Core\Classes;
-
-class Router
-{
-    // Array der Routen
-    protected $routes = [];
-
-    // Array der Parameter
-    protected $params = [];
-
-    // Füge eine Route zum Array der Routen hinzu
-    public function add($route, $params = [])
+    // Konstruktor der Klasse Router
+    public function __construct()
     {
-        // Konvertiere die Route zu einem regulären Ausdruck: entkomme Schrägstrichen
-        $route = preg_replace('/\//', '\\/', $route);
-
-        // Konvertiere Variablen z.B. {controller}
-        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
-
-        // Konvertiere Variablen mit benutzerdefinierten regulären Ausdrücken z.B. {id:\d+}
-        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
-
-        // Füge Start- und Endzeichen hinzu und markiere den Parameter als optional
-        $route = '/^' . $route . '((\\?P[a-z-]+)\\?)?$/i';
-
-        // Füge die Route zum Array der Routen hinzu
-        $this->routes[$route] = $params;
+        // Erstelle ein Objekt der Klasse RouteManager
+        $this->routeManager = new RouteManager();
     }
 
-    // Überprüfe, ob die URL mit einer Route übereinstimmt und rufe die entsprechende Funktion auf
-    public function dispatch($url_path)
+    // Methode zum Überprüfen, ob die URL mit einer Route übereinstimmt und zum Aufrufen der entsprechenden Funktion
+    public function dispatch()
     {
+        // Hole die URL aus dem Server-Array
+        $url = $_SERVER['REQUEST_URI'];
+
         // Entferne alle führenden und nachfolgenden Schrägstriche aus der URL
-        $url_path = trim($url_path, '/');
+        $url = trim($url, '/');
 
-        // Überprüfe, ob die Route übereinstimmt
-        foreach ($this->routes as $route => $params) {
-            if (preg_match($route, $url_path, $matches)) {
-                // Hole alle benannten Parameter aus dem Array
-                foreach ($matches as $key => $match) {
-                    if (is_string($key)) {
-                        $params[$key] = $match;
-                    }
-                }
+        // Hole alle Routen aus dem Objekt routeManager
+        $routes = $this->routeManager->getRoutes();
 
-                // Setze die Parameter für den Router
-                $this->params = $params;
+        // Überprüfe jede Route auf Übereinstimmung mit der URL
+        foreach ($routes as $route) {
+            // Hole den Pfad, den Controller und die Methode aus der Route
+            $path = $route['path'];
+            $controller = $route['controller'];
+            $method = $route['method'];
 
-                // Rufe die Funktion auf, die der Route zugeordnet ist
-                call_user_func($this->routes[$route]);
+            // Erstelle einen regulären Ausdruck aus dem Pfad
+            $regex = '/^' . str_replace('/', '\/', $path) . '$/';
 
-                return true;
+            // Überprüfe, ob der reguläre Ausdruck mit der URL übereinstimmt
+            if (preg_match($regex, $url, $matches)) {
+                // Entferne den ersten Eintrag aus dem Array der Übereinstimmungen
+                array_shift($matches);
+
+                // Lade die Datei, die den Controller enthält
+                require_once 'controllers/' . $controller . '.php';
+
+                // Erstelle ein Objekt des Controllers
+                $controller = new $controller();
+
+                // Rufe die Methode auf dem Objekt auf und übergebe die Übereinstimmungen als Parameter
+                call_user_func_array([$controller, $method], $matches);
+
+                // Beende die Schleife
+                break;
             }
         }
-
-        return false;
-    }
-
-
-    // Gib das Array der Parameter zurück
-    public function getParams()
-    {
-        // Hole die Parameter aus dem Router
-        $params = $this->params;
-
-        // Überprüfe, ob ein Parameter in der URL vorhanden ist
-        if (isset($params['?P'])) {
-            // Entferne das Fragezeichen und den Buchstaben P aus dem Parameter
-            $params['?P'] = str_replace('?P', '', $params['?P']);
-
-            // Füge den Parameter zum Array der Parameter hinzu
-            $params[] = $params['?P'];
-
-            // Entferne den Parameter mit dem Schlüssel '?P' aus dem Array
-            unset($params['?P']);
-        }
-
-        // Gib das Array der Parameter zurück
-        return $params;
-    }
-}
-
-    // Füge eine Route zum Array der Routen hinzu
-    public function add($route, $params = [])
-    {
-        // Konvertiere die Route zu einem regulären Ausdruck: entkomme Schrägstrichen
-        $route = preg_replace('/\//', '\\/', $route);
-
-        // Konvertiere Variablen z.B. {controller}
-        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
-
-        // Konvertiere Variablen mit benutzerdefinierten regulären Ausdrücken z.B. {id:\d+}
-        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
-
-        // Füge Start- und Endzeichen hinzu und markiere den Parameter als optional
-        $route = '/^' . $route . '((\\?P[a-z-]+)\\?)?$/i';
-
-        // Füge die Route zum Array der Routen hinzu
-        $this->routes[$route] = $params;
-    }
-
-    // Überprüfe, ob die URL mit einer Route übereinstimmt und rufe die entsprechende Funktion auf
-    public function dispatch($url_path)
-    {
-        // Entferne alle führenden und nachfolgenden Schrägstriche aus der URL
-        $url_path = trim($url_path, '/');
-
-        // Überprüfe, ob die Route übereinstimmt
-        foreach ($this->routes as $route => $params) {
-            if (preg_match($route, $url_path, $matches)) {
-                // Hole alle benannten Parameter aus dem Array
-                foreach ($matches as $key => $match) {
-                    if (is_string($key)) {
-                        $params[$key] = $match;
-                    }
-                }
-
-                // Setze die Parameter für den Router
-                $this->params = $params;
-
-                // Rufe die Funktion auf, die der Route zugeordnet ist
-                call_user_func($this->routes[$route]);
-
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    // Gib das Array der Parameter zurück
-    public function getParams()
-    {
-        // Hole die Parameter aus dem Router
-        $params = $this->params;
-
-        // Überprüfe, ob ein Parameter in der URL vorhanden ist
-        if (isset($params['?P'])) {
-            // Entferne das Fragezeichen und den Buchstaben P aus dem Parameter
-            $params['?P'] = str_replace('?P', '', $params['?P']);
-
-            // Füge den Parameter zum Array der Parameter hinzu
-            $params[] = $params['?P'];
-
-            // Entferne den Parameter mit dem Schlüssel '?P' aus dem Array
-            unset($params['?P']);
-        }
-
-        // Gib das Array der Parameter zurück
-        return $params;
     }
 }
